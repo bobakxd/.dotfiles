162d0e2a5dd63669bae818bc84eeab7089878672
Roman Mazalov <imspeedwagon@gmail.com>
2022-12-05 02:47:40 +0300

    Add emacs config

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,20 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("05626f77b0c8c197c7e4a31d9783c4ec6e351d9624aa28bc15e7f6d6a6ebd926" default))
+ '(package-selected-packages '(dracula-theme))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "mononoki" :foundry "NONE" :slant normal :weight normal :height 158 :width normal)))))
+
+(tool-bar-mode -1)
